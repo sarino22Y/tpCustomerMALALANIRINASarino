fe612f5d1ab3b7e668645ed67dc44e60f22d1aa1
Sarino MALALANIRINA <sarino.malalanirna@esti.mg>
2023-03-24 23:55:50 +0300

    Ajout d'un backing bean pour la page des détails (CustomerDetailsBean)

--- tpCustomerMALALANIRINASarino/src/main/java/mg/itu/tpcustomermalalanirinasarino/ejb/CustomerManager.java
+++ tpCustomerMALALANIRINASarino/src/main/java/mg/itu/tpcustomermalalanirinasarino/ejb/CustomerManager.java
@@ -32,4 +32,8 @@ public class CustomerManager {
     public Customer update(Customer customer) {
         return em.merge(customer);  
     }         
+
+    public Customer findById(int idCustomer) {
+        return em.find(Customer.class, idCustomer);
+    }
 }
